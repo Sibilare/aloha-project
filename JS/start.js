41002ea7242617d7427d0d0db4fd60223f41d987
Stewart Point <photobystewart@gmail.com>
2019-03-06 14:26:15 -0800

    email validator

--- JS/start.js
+++ JS/start.js
@@ -20,3 +20,29 @@ $(function() {
       ).focus();
     });
 });
+$(document).ready(function() {
+	/* Toggle nav bar menu on click for mobile devices */
+    $("#navToggle a").click(function(e){
+        e.preventDefault();
+        
+        $("header > nav").slideToggle();
+        $("header > div").toggleClass("menuUp menuDown");
+		
+	});
+    
+	$(".navicon-button").click(function(){
+		$(this).toggleClass("open");
+	});
+});
+
+
+$(".ebutton").click(function(validateEmail){
+  return validateEmail;
+})
+/* taken from https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript */
+/* minor change from 'email' to 'emailinput' */
+
+function validateEmail(emailinput) {
+  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
